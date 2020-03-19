fa279975af71d4edbc83bed4d6ebaf5a81a7685a
Paulo Oliveira <paulooliveira@pecege.com>
2020-03-19 17:11:53 -0300

    add repo

---
+++ AvisaEuEuEu/index.js
@@ -0,0 +1,28 @@
+const { WebClient } = require('@slack/web-api');
+
+const dayOfWeekNames = {
+    1: 'Tâco',
+    2: 'Têco',
+    3: 'Tíco',
+    4: 'Tôco',
+    5: 'Tûco'
+};
+
+module.exports = async function (context, myTimer) {
+    const web = new WebClient(process.env['SLACK_API_TOKEN']);
+    
+    if (myTimer.isPastDue)
+        context.log('JavaScript is running late!');
+
+    const weekDay = new Date().getDay();
+    const text = `Olá *${dayOfWeekNames[weekDay-1]}*! \nTo passando aqui só pra lembrar que ta na hora de trocar o nome para *${dayOfWeekNames[weekDay]}*. \n :squirrel::shipit:`;
+
+    const result = await web.chat.postMessage({
+        text: text,
+        channel: process.env['SLACK_IM_TO_POST'],
+    });
+    
+    // The result contains an identifier for the message, `ts`.
+    context.log(`Successfully send message: ${JSON.stringify(result)}`);
+    context.done();
+};
